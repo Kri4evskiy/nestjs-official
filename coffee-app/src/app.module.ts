808db2503a9b332bac44982c134464bcade62f45
Stanislav <kri4evskiy@gmail.com>
2026-09-17 19:38:16 +0300

    refactor: guard auth with custom public decorator and injected ConfigService

--- coffee-app/src/app.module.ts
+++ coffee-app/src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { CoffeesModule } from './coffees/coffees.module';
+import { CommonModule } from './common/common.module';
 import appConfig from './config/app.config';
 
 @Module({
@@ -40,18 +41,9 @@ import appConfig from './config/app.config';
     }),
     CoffeesModule,
     CoffeeRatingModule,
+    CommonModule,
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    // {
-    //   provide: APP_PIPE,
-    //   useClass: ValidationPipe,
-    // },
-    // {
-    //   provide: APP_GUARD,
-    //   useClass: ApiKeyGuard,
-    // },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
