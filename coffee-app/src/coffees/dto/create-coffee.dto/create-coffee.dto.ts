5b83bce1c53dff1139c317c3b8852b1bec6707d5
Stanislav <kri4evskiy@gmail.com>
2026-09-17 16:43:59 +0300

    fix: add description fielt into coffee.entity

--- coffee-app/src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
+++ coffee-app/src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
@@ -1,9 +1,13 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
   @IsString()
   @IsNotEmpty()
   readonly name: string;
+
+  @IsOptional()
+  @IsString()
+  readonly description?: string;
 
   @IsString()
   @IsNotEmpty()
