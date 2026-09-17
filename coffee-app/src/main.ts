591988892538bb16270ff7b4b97dd0c0c5cf6b5a
Stanislav <kri4evskiy@gmail.com>
2026-09-17 17:56:37 +0300

    feat: implement global http exeption filter

--- coffee-app/src/main.ts
+++ coffee-app/src/main.ts
@@ -1,23 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // 1. Очистка от лишнего: удаляет из входящего JSON поля, которых нет в DTO
-      forbidNonWhitelisted: true, // 2. Строгий режим: если прислали лишнее поле, не просто удалять, а выбрасывать 400 ошибку
-      transform: true, // 3. Авто-трансформация типов:
-      // - Превращает '123' из URL (@Param('id')) в число 123, если в TS указано id: number
-      // - Превращает plain JSON в настоящий инстанс класса DTO (new CreateCoffeeDto())
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
       transformOptions: {
-        // 4. Опции трансформации (например, авто-приведение query/param типов к примитивам)
         enableImplicitConversion: true,
       },
       disableErrorMessages: process.env.NODE_ENV === 'production',
     }),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 
