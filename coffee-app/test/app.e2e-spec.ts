7aaa028640b63aff023eab80b91b5bb934399531
Stanislav <kri4evskiy@gmail.com>
2026-09-20 15:37:13 +0300

    test: e-2-e set up

--- coffee-app/test/app.e2e-spec.ts
+++ coffee-app/test/app.e2e-spec.ts
@@ -1,13 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+  let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -17,13 +16,14 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return request(app.getHttpServer()) // 👈
       .get('/')
+      .set('Authorization', process.env.API_KEY || '') // 👈
       .expect(200)
-      .expect('Hello World!');
+      .expect('Hello Nest!!');
   });
 
-  afterEach(async () => {
+  afterAll(async () => {
     await app.close();
   });
 });
